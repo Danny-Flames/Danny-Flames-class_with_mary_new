e0d402b69432a5e0594af490099dead70656caec
DaNny <danniskolobosiky@gmail.com>
2024-06-22 18:38:46 +0100

    latest changes

--- Day_7__22-06-2024/app_3.js
+++ Day_7__22-06-2024/app_3.js
@@ -31,7 +31,7 @@ function addNum() {
 
 console.log(addNum())
 
-// Class work
+// Class work -1
 // Create a function of any name, create 3 variables holding Number data type, using addition sign(+)
 // and subtraction sign(-) to do the computation.  eg:  12 + 4 - 6, return the value of the computation 
 // in the function and ensure to log this returned value to the console.
@@ -44,3 +44,31 @@ function maryGeorge() {
 }
 
 console.log(maryGeorge())
+
+// Function with arguments/parameters
+// Parameters are values/variables the function depends on during declaration.
+// Arguments are values/variables that must be passed into the function when it is invoked(called).
+
+function addTwoNums(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+console.log(addTwoNums(4, 5))
+console.log(addTwoNums(12, 6))
+console.log(addTwoNums(100, 200))
+
+
+function displayUserNameAndAge(name, age) {
+    let userSentence = 'My name is ' + name + '. I am ' + age + 'yrs old.';
+    return userSentence;
+}
+
+console.log(displayUserNameAndAge('Mary', 4))
+console.log(displayUserNameAndAge('Samson', 2))
+console.log(displayUserNameAndAge('Samuel', 6))
+
+// Class work-2
+// Write a student program that accepts four arguments:  name, school, course, currentLevel and then print a 
+// valid sentence telling the user his/her full details based off the arguments passed.
+
